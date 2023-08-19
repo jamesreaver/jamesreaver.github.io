19c656f459934126033de4415d4b1861e1dc087f
jameshreaver <10749703+jameshreaver@users.noreply.github.com>
2023-08-19 13:14:10 +0100

    v1.2.4

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,3 +1,4 @@
+import NoSsr from '@/components/NoSsr'
 import { randomQuote } from '@/lib/randomQuote'
 import { useMemo } from 'react'
 
@@ -7,9 +8,11 @@ export function Quote() {
   return (
     <div className="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
       <div className="md:col-span-3 md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-        <p className="font-serif text-base text-zinc-600 dark:text-zinc-400" suppressHydrationWarning>
-          “{quote?.text}”<br/>— {quote.author}
-        </p>
+        <NoSsr>
+          <p className="font-serif text-base text-zinc-600 dark:text-zinc-400">
+            “{quote?.text}”<br/>— {quote.author}
+          </p>
+        </NoSsr>
       </div>
     </div>
   )
